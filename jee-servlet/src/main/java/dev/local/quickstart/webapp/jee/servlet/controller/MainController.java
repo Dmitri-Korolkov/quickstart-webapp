0b5bb06790195c4da76ad8c900f0ff713df4d044
Dmitri-Korolkov <d.korolkov88@gmail.com>
2016-11-22 14:12:32 +0300

    update jee servlet module

---
+++ jee-servlet/src/main/java/dev/local/quickstart/webapp/jee/servlet/controller/MainController.java
@@ -0,0 +1,37 @@
+package dev.local.quickstart.webapp.jee.servlet.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainController extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+          throws ServletException, IOException {
+
+    List<String> books = new ArrayList<>();
+    books.add("Reactive Java Programming");
+    books.add("Java Performance Companion");
+    books.add("Java The Complete Reference, 8th Edition");
+    books.add("Building Isomorphic JavaScript Apps");
+    books.add("Absolute Java, 6th Edition");
+
+    req.setAttribute("books", books);
+
+    req.getRequestDispatcher("/WEB-INF/view/html.jsp").forward(req, resp);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+          throws ServletException, IOException {
+
+    super.doPost(req, resp);
+  }
+
+
+}
---
+++ jee-servlet/src/main/webapp/WEB-INF/view/html.jsp
@@ -0,0 +1,14 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<ul>
+    <c:forEach items="${books}" var="item">
+        <li>${item}</li>
+    </c:forEach>
+</ul>
+</body>
+</html>
