87fe04fccd9d6fb2482851ec639983b01235fdbf
Dmitri-Korolkov <d.korolkov88@gmail.com>
2016-11-22 11:39:19 +0300

    add zurb foundation

--- spring-freemarker/src/main/java/dev/local/quickstart/webapp/spring/freemarker/controller/MainController.java
+++ spring-freemarker/src/main/java/dev/local/quickstart/webapp/spring/freemarker/controller/MainController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -18,7 +20,14 @@ public class MainController {
   @RequestMapping(value = "/**", method = RequestMethod.GET)
   public String mainReq(ModelMap model, HttpServletRequest request, HttpServletResponse response) {
 
+    List<String> books = new ArrayList<>();
+    books.add("Reactive Java Programming");
+    books.add("Java Performance Companion");
+    books.add("Java The Complete Reference, 8th Edition");
+    books.add("Building Isomorphic JavaScript Apps");
+    books.add("Absolute Java, 6th Edition");
 
+    model.put("books", books);
 
     return "html";
   }
