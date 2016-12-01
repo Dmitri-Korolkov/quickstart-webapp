c2d30b01ea9e72603cd3624db12bf547dab863d5
Dmitri-Korolkov <d.korolkov88@gmail.com>
2016-12-01 16:59:38 +0300

    add web socket

---
+++ jee-servlet/src/main/java/dev/local/quickstart/webapp/jee/servlet/controller/WSController.java
@@ -0,0 +1,53 @@
+package dev.local.quickstart.webapp.jee.servlet.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.util.*;
+
+@ServerEndpoint("/ws")
+public class WSController {
+
+  private static final Logger log = LoggerFactory.getLogger(WSController.class);
+
+  private static Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
+
+  @OnOpen
+  public void onOpen(Session peer) {
+    peers.add(peer);
+  }
+
+  @OnClose
+  public void onClose(Session peer) {
+    peers.remove(peer);
+  }
+
+
+  @OnMessage
+  public String onMessage(String message) {
+    callSessions(message);
+    return null;
+  }
+
+
+  private void callSessions(String msg) {
+
+    Iterator<Session> iterator = peers.iterator();
+
+    while (iterator.hasNext()) {
+      Session session = iterator.next();
+      try {
+        if (session.isOpen()) session.getBasicRemote().sendText(msg);
+        else peers.remove(session);
+      } catch (Exception e) {
+        log.error("call peer error: {}", e);
+      }
+    }
+  }
+
+}
--- jee-servlet/src/main/webapp/WEB-INF/view/html.jsp
+++ jee-servlet/src/main/webapp/WEB-INF/view/html.jsp
@@ -3,6 +3,29 @@
 <html>
 <head>
     <title>Title</title>
+
+
+    <script>
+        var ws = new WebSocket("ws://localhost:8888/ws");
+        ws.onopen = function () {
+            console.log("ws is open");
+        };
+        ws.onmessage = function (message) {
+            document.getElementById("chatlog").textContent += message.data + "\n";
+        };
+        function postToServer() {
+            ws.send(document.getElementById("msg").value);
+            document.getElementById("msg").value = "";
+        }
+        function closeConnect() {
+            console.log("ws is closed");
+            ws.close();
+        }
+
+
+
+    </script>
+
 </head>
 <body>
 <ul>
@@ -10,5 +33,10 @@
         <li>${item}</li>
     </c:forEach>
 </ul>
+<textarea id="chatlog" readonly></textarea><br/>
+<input id="msg" type="text"/>
+<button type="submit" id="sendButton2" onClick="postToServer()">Send!</button>
+<button type="submit" id="sendButton3" onClick="closeConnect()">End</button>
+
 </body>
 </html>
